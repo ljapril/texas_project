0dd28836a514ce7376c9754fc97d120af20f44d7
GarciaMarc41 <87959387+GarciaMarc41@users.noreply.github.com>
2023-03-31 17:23:48 -0600

    Add files via upload

---
+++ Preprocessing/create_fname_list.py
@@ -0,0 +1,34 @@
+#import libraries
+from pathlib import Path
+import numpy as np
+import os
+from collections import namedtuple
+import pandas as pd
+
+File = namedtuple('File','fname')
+
+#empty list
+files =[]
+
+#create starting path
+p = Path('/nfs/data/isml/Marc/PhaseNet/ElPaso/data')
+
+
+#Iterate through path to find all seed files
+for item in p.glob('**/*'):
+    if item.suffix in ['.mseed']:
+        fname = item.name
+        #path = Path.resolve(item).parent
+    
+        files.append(File(fname))
+
+ 
+
+#Creating a dataframe        
+df = pd.DataFrame(files)
+
+#Sort Files
+sorted_df = df.sort_values(by=["fname"], ascending=True)
+
+#Saving dataframe as a CSV
+sorted_df.to_csv(r'/nfs/data/isml/Marc/PhaseNet/ElPaso/data/fnames.csv', index = False)
